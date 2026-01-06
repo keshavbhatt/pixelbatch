f243567dedf44164f7f64d130fa64eddd75f0e09
Keshav Bhatt <keshavnrj@gmail.com>
2026-01-06 19:31:21 +0530

    feat: implement size persistence for optimizer dialog on close

--- src/OptimizerPrefWidgets/imageformatprefwidgetfactory.cpp
+++ src/OptimizerPrefWidgets/imageformatprefwidgetfactory.cpp
@@ -5,11 +5,37 @@
 #include "svgoprefwidget.h"
 #include "worker/imageworkerfactory.h"
 #include <QDebug>
+#include <QEvent>
 #include <QHBoxLayout>
 #include <QMessageBox>
 #include <QPushButton>
+#include <QResizeEvent>
 #include <QScrollArea>
+#include <QSettings>
 #include <QVBoxLayout>
+
+// Event filter to save optimizer dialog size on close
+class OptimizerDialogEventFilter : public QObject {
+public:
+  OptimizerDialogEventFilter(QScrollArea *parent, const QString &optimizerName)
+      : QObject(parent), m_optimizerName(optimizerName), m_scrollArea(parent) {}
+
+protected:
+  bool eventFilter(QObject *obj, QEvent *event) override {
+    if (event->type() == QEvent::Close && m_scrollArea) {
+      // Save the current size before closing
+      QSettings settings;
+      QString sizeKey = QString("OptimizerDialog/%1/size").arg(m_optimizerName);
+      settings.setValue(sizeKey, m_scrollArea->size());
+      qDebug() << "Saved" << m_optimizerName << "dialog size:" << m_scrollArea->size();
+    }
+    return QObject::eventFilter(obj, event);
+  }
+
+private:
+  QString m_optimizerName;
+  QScrollArea *m_scrollArea;
+};
 
 ImageFormatPrefWidgetFactory &ImageFormatPrefWidgetFactory::instance() {
   static ImageFormatPrefWidgetFactory instance;
@@ -33,10 +59,42 @@ void ImageFormatPrefWidgetFactory::openPrefWidgetFor(
       scrollArea->setWidget(widget);
       scrollArea->setWidgetResizable(true);
       scrollArea->setWindowTitle(optimizerName + " " + QObject::tr("Preferences"));
+      scrollArea->setAttribute(Qt::WA_DeleteOnClose);
 
       widget->loadSettings();
+
+      // Set a better initial size based on content
+      widget->adjustSize();
+      QSize widgetSize = widget->sizeHint();
+
+      // Set minimum size to ensure usability
+      int minWidth = 500;
+      int minHeight = 400;
+
+      // Calculate initial size (content size + some padding for scroll area)
+      int initialWidth = qMax(minWidth, widgetSize.width() + 40);
+      int initialHeight = qMax(minHeight, widgetSize.height() + 40);
+
+      // Restore last user-set size from settings
+      QSettings settings;
+      QString sizeKey = QString("OptimizerDialog/%1/size").arg(optimizerName);
+      QSize savedSize = settings.value(sizeKey, QSize(initialWidth, initialHeight)).toSize();
+
+      // Apply the size (use saved size if available, otherwise use calculated initial size)
+      scrollArea->resize(savedSize);
+
+      // Save size when window is closed
+      QObject::connect(scrollArea, &QScrollArea::destroyed, [optimizerName]() {
+        // Note: We can't access scrollArea->size() here as it's being destroyed
+        // The size is saved in closeEvent via eventFilter instead
+      });
+
+      // Install event filter to save size on close
+      scrollArea->installEventFilter(new OptimizerDialogEventFilter(scrollArea, optimizerName));
+
       scrollArea->show();
-      qDebug() << "Opened preference widget for" << optimizerName;
+      qDebug() << "Opened preference widget for" << optimizerName
+               << "with size:" << scrollArea->size();
     }
   } else {
     qDebug() << "No preference widget registered for optimizer:"
